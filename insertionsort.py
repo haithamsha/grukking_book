8116cd0f067844a4231b0eb8b2cb94a17b4d4f4b
Haitham Shaaban <haithamshaabann@gmail.com>
2021-09-30 15:18:48 +0200

    insertion sort

---
+++ insertionsort.py
@@ -0,0 +1,25 @@
+#get the smallest number
+#create new list
+#put the small number in the new list one by one
+
+
+def get_smallest_num(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def insertion_sort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = get_smallest_num(arr)
+        newArr.append(arr[smallest])
+    return newArr
+
+
+arr_test = [5, 3, 2, 10]
+
+print(insertion_sort(arr_test))
